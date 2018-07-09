ecd11da8d246d33135c570b93684851e16b8bda2
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-07-09 22:25:23 +0300

    Assigment 2 - Question 1

--- handler.js
+++ handler.js
@@ -1,20 +1,44 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+function saveNameToTable(name) {
+	const item = {};
+	item.name = name;
+
+	const params = {
+		TableName: 'GreetNames',
+		Item: item
+	};
+
+	return dynamo.put(params).promise();
+}
+
+function sendResponse(statusCode, message, callback) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
+}
+
 module.exports.hello = (event, context, callback) => {
 	var message = 'Hello World';
 
 	const name = event.queryStringParameters && event.queryStringParameters.name;
 
 	if (name !== null) {
-		message = 'Hello ' + event.queryStringParameters.name;
+		message = 'Hello ' + name;
+
+		saveNameToTable(name)
+			.then(() => {
+				sendResponse(200, message, callback);
+			})
+			.catch(error => {
+				console.log(error);
+				sendResponse(500, error, callback);
+			});
 	}
-
-	const response = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: message
-		})
-	};
-
-	callback(null, response);
+	sendResponse(200, message, callback);
 };
