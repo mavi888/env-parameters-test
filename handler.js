64ddcfc3e0fb8f6537c167cddca6be0ece7f3469
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-07-09 22:10:47 +0300

    Assigment 1 - Question 2

--- handler.js
+++ handler.js
@@ -1,10 +1,18 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+	var message = 'Hello World';
+
+	const name = event.queryStringParameters && event.queryStringParameters.name;
+
+	if (name !== null) {
+		message = 'Hello ' + event.queryStringParameters.name;
+	}
+
 	const response = {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: 'Hello World'
+			message: message
 		})
 	};
 
